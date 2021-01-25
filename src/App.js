a3f4448c2a3f4428bed71c3122f5e195d9e00db9
윤석 한 <hys6078@naver.com>
2021-01-25 15:56:52 +0900

    Practice State, class, props of React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
+function Food({name, picture, rating}){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name}/>
     </div>
   );
 }
 
+Food.propTypes={
+  name:PropTypes.string.isRequired,
+  picture:PropTypes.string.isRequired,
+  rating:PropTypes.number.isRequired
+}
+
+class App extends React.Component{
+  state={
+    count:0
+  }
+  add=()=>{
+    this.setState(current=>({count:current.count+1}))
+  };
+  minus=()=>{
+    this.setState(current=>({count:current.count-1}))
+  };
+  render(){
+    return (
+    <div>
+      <h1>the number is {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+    </div>);
+  }
+}
 export default App;
